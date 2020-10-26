7eb6d6a50b3a41bf88d97a0a6ab5e3b5d2e62135
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-26 04:02:50 +0000

    Update DartsGame.sln

--- DartsGame/Darts.cs
+++ DartsGame/Darts.cs
@@ -4,15 +4,26 @@ namespace DartsGame
 {
     public static class Darts
     {
-        /// <summary>
-        /// Calculates the earned points in a single toss of a Darts game.
-        /// </summary>
-        /// <param name="x">x-coordinate of dart.</param>
-        /// <param name="y">y-coordinate of dart.</param>
-        /// <returns>The earned points.</returns>
+        
         public static int GetScore(double x, double y)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            double r = Math.Sqrt((x * x) + (y * y));
+            if (r <= 1)
+            {
+                return 10;
+            }
+
+            if (r <= 5)
+            {
+                return 5;
+            }
+
+            if (r <= 10)
+            {
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
